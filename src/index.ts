365f6199871036a37aba66240ab67afc1f02475d
JureKotnik <jure2002k@gmail.com>
2023-02-21 17:35:29 +0100

    arrays

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-let sales = 123_456_789;
-let course ='typeScript';
-let is_published = true;
-let level;
-
-function render(document){
-    console.log(document);
-}
+let numbers: number[] = [];
+numbers[0]=1;
